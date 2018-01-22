4bf4823be75a5c2d0240b5ab5bd467735a660a29
gngdb <gavingray1729@gmail.com>
2018-01-22 11:59:34 +0000

    refactor out redundant ReductionCell0/1 and BranchSeparableReduction

--- models/nasnet.py
+++ models/nasnet.py
@@ -62,31 +62,37 @@ pretrained_settings = {
     }
 }
 
-class MaxPoolPad(nn.Module):
+class MaxPool(nn.Module):
 
-    def __init__(self):
-        super(MaxPoolPad, self).__init__()
-        self.pad = nn.ZeroPad2d((1, 0, 1, 0))
+    def __init__(self, pad=False):
+        super(MaxPool, self).__init__()
+        self.pad = pad
+        self.pad = nn.ZeroPad2d((1, 0, 1, 0)) if pad else None
         self.pool = nn.MaxPool2d(3, stride=2, padding=1)
 
     def forward(self, x):
-        x = self.pad(x)
+        if self.pad:
+            x = self.pad(x)
         x = self.pool(x)
-        x = x[:, :, 1:, 1:]
+        if self.pad:
+            x = x[:, :, 1:, 1:]
         return x
 
 
-class AvgPoolPad(nn.Module):
+class AvgPool(nn.Module):
 
-    def __init__(self, stride=2, padding=1):
-        super(AvgPoolPad, self).__init__()
-        self.pad = nn.ZeroPad2d((1, 0, 1, 0))
-        self.pool = nn.AvgPool2d(3, stride=stride, padding=padding, count_include_pad=False)
+    def __init__(self, pad=False, stride=2, padding=1):
+        super(AvgPool, self).__init__()
+        self.pad = pad
+        self.pad = nn.ZeroPad2d((1, 0, 1, 0)) if pad else None
+        self.pool = nn.AvgPool2d(3, stride=stride, padding=padding)
 
     def forward(self, x):
-        x = self.pad(x)
+        if self.pad:
+            x = self.pad(x)
         x = self.pool(x)
-        x = x[:, :, 1:, 1:]
+        if self.pad:
+            x = x[:, :, 1:, 1:]
         return x
 
 
@@ -109,7 +115,7 @@ class SeparableConv2d(nn.Module):
 
 class BranchSeparables(nn.Module):
 
-    def __init__(self, in_channels, out_channels, kernel_size, stride, padding, bias=False):
+    def __init__(self, in_channels, out_channels, kernel_size, stride, padding, bias=False, reduction=False, z_padding=1):
         super(BranchSeparables, self).__init__()
         self.relu = nn.ReLU()
         self.separable_1 = SeparableConv2d(in_channels, in_channels, kernel_size, stride, padding, bias=bias)
@@ -117,15 +123,20 @@ class BranchSeparables(nn.Module):
         self.relu1 = nn.ReLU()
         self.separable_2 = SeparableConv2d(in_channels, out_channels, kernel_size, 1, padding, bias=bias)
         self.bn_sep_2 = nn.BatchNorm2d(out_channels, eps=0.001, momentum=0.1, affine=True)
+        if reduction:
+            self.padding = nn.ZeroPad2d((z_padding, 0, z_padding, 0))
 
     def forward(self, x):
         x = self.relu(x)
+        x = self.padding(x) if hasattr(self, 'padding') else x
         x = self.separable_1(x)
+        x = x[:, :, 1:, 1:].contiguous() if hasattr(self, 'padding') else x
         x = self.bn_sep_1(x)
         x = self.relu1(x)
         x = self.separable_2(x)
         x = self.bn_sep_2(x)
         return x
+
 
 
 class BranchSeparablesStem(nn.Module):
@@ -142,24 +153,6 @@ class BranchSeparablesStem(nn.Module):
     def forward(self, x):
         x = self.relu(x)
         x = self.separable_1(x)
-        x = self.bn_sep_1(x)
-        x = self.relu1(x)
-        x = self.separable_2(x)
-        x = self.bn_sep_2(x)
-        return x
-
-
-class BranchSeparablesReduction(BranchSeparables):
-
-    def __init__(self, in_channels, out_channels, kernel_size, stride, padding, z_padding=1, bias=False):
-        BranchSeparables.__init__(self, in_channels, out_channels, kernel_size, stride, padding, bias)
-        self.padding = nn.ZeroPad2d((z_padding, 0, z_padding, 0))
-
-    def forward(self, x):
-        x = self.relu(x)
-        x = self.padding(x)
-        x = self.separable_1(x)
-        x = x[:, :, 1:, 1:].contiguous()
         x = self.bn_sep_1(x)
         x = self.relu1(x)
         x = self.separable_2(x)
@@ -371,10 +364,10 @@ class NormalCell(nn.Module):
         return x_out
 
 
-class ReductionCell0(nn.Module):
+class ReductionCell(nn.Module):
 
-    def __init__(self, in_channels_left, out_channels_left, in_channels_right, out_channels_right):
-        super(ReductionCell0, self).__init__() 
+    def __init__(self, in_channels_left, out_channels_left, in_channels_right, out_channels_right, pad=False):
+        super(ReductionCell, self).__init__() 
         self.conv_prev_1x1 = nn.Sequential()
         self.conv_prev_1x1.add_module('relu', nn.ReLU())
         self.conv_prev_1x1.add_module('conv', nn.Conv2d(in_channels_left, out_channels_left, 1, stride=1, bias=False))
@@ -385,74 +378,19 @@ class ReductionCell0(nn.Module):
         self.conv_1x1.add_module('conv', nn.Conv2d(in_channels_right, out_channels_right, 1, stride=1, bias=False))
         self.conv_1x1.add_module('bn', nn.BatchNorm2d(out_channels_right, eps=0.001, momentum=0.1, affine=True))
 
-        self.comb_iter_0_left = BranchSeparablesReduction(out_channels_right, out_channels_right, 5, 2, 2, bias=False)
-        self.comb_iter_0_right = BranchSeparablesReduction(out_channels_right, out_channels_right, 7, 2, 3, bias=False)
+        self.comb_iter_0_left = BranchSeparables(out_channels_right, out_channels_right, 5, 2, 2, bias=False, reduction=pad)
+        self.comb_iter_0_right = BranchSeparables(out_channels_right, out_channels_right, 7, 2, 3, bias=False, reduction=pad)
+        
+        self.comb_iter_1_left = MaxPool(pad=pad)
+        self.comb_iter_1_right = BranchSeparables(out_channels_right, out_channels_right, 7, 2, 3, bias=False, reduction=pad)
 
-        self.comb_iter_1_left = MaxPoolPad()
-        self.comb_iter_1_right = BranchSeparablesReduction(out_channels_right, out_channels_right, 7, 2, 3, bias=False)
-
-        self.comb_iter_2_left = AvgPoolPad()
-        self.comb_iter_2_right = BranchSeparablesReduction(out_channels_right, out_channels_right, 5, 2, 2, bias=False)
-
-        self.comb_iter_3_right = nn.AvgPool2d(3, stride=1, padding=1, count_include_pad=False)
-
-        self.comb_iter_4_left = BranchSeparablesReduction(out_channels_right, out_channels_right, 3, 1, 1, bias=False)
-        self.comb_iter_4_right = MaxPoolPad()
-
-    def forward(self, x, x_prev):
-        x_left = self.conv_prev_1x1(x_prev)
-        x_right = self.conv_1x1(x)
-
-        x_comb_iter_0_left = self.comb_iter_0_left(x_right)
-        x_comb_iter_0_right = self.comb_iter_0_right(x_left)
-        x_comb_iter_0 = x_comb_iter_0_left + x_comb_iter_0_right
-
-        x_comb_iter_1_left = self.comb_iter_1_left(x_right)
-        x_comb_iter_1_right = self.comb_iter_1_right(x_left)
-        x_comb_iter_1 = x_comb_iter_1_left + x_comb_iter_1_right
-
-        x_comb_iter_2_left = self.comb_iter_2_left(x_right)
-        x_comb_iter_2_right = self.comb_iter_2_right(x_left)
-        x_comb_iter_2 = x_comb_iter_2_left + x_comb_iter_2_right
-
-        x_comb_iter_3_right = self.comb_iter_3_right(x_comb_iter_0)
-        x_comb_iter_3 = x_comb_iter_3_right + x_comb_iter_1
-
-        x_comb_iter_4_left = self.comb_iter_4_left(x_comb_iter_0)
-        x_comb_iter_4_right = self.comb_iter_4_right(x_right)
-        x_comb_iter_4 = x_comb_iter_4_left + x_comb_iter_4_right
-
-        x_out = torch.cat([x_comb_iter_1, x_comb_iter_2, x_comb_iter_3, x_comb_iter_4], 1)
-        return x_out
-
-
-class ReductionCell1(nn.Module):
-
-    def __init__(self, in_channels_left, out_channels_left, in_channels_right, out_channels_right):
-        super(ReductionCell1, self).__init__()
-        self.conv_prev_1x1 = nn.Sequential()
-        self.conv_prev_1x1.add_module('relu', nn.ReLU())
-        self.conv_prev_1x1.add_module('conv', nn.Conv2d(in_channels_left, out_channels_left, 1, stride=1, bias=False))
-        self.conv_prev_1x1.add_module('bn', nn.BatchNorm2d(out_channels_left, eps=0.001, momentum=0.1, affine=True))
-
-        self.conv_1x1 = nn.Sequential()
-        self.conv_1x1.add_module('relu', nn.ReLU())
-        self.conv_1x1.add_module('conv', nn.Conv2d(in_channels_right, out_channels_right, 1, stride=1, bias=False))
-        self.conv_1x1.add_module('bn', nn.BatchNorm2d(out_channels_right, eps=0.001, momentum=0.1, affine=True))
-
-        self.comb_iter_0_left = BranchSeparables(out_channels_right, out_channels_right, 5, 2, 2, bias=False)
-        self.comb_iter_0_right = BranchSeparables(out_channels_right, out_channels_right, 7, 2, 3, bias=False)
-
-        self.comb_iter_1_left = nn.MaxPool2d(3, stride=2, padding=1)
-        self.comb_iter_1_right = BranchSeparables(out_channels_right, out_channels_right, 7, 2, 3, bias=False)
-
-        self.comb_iter_2_left = nn.AvgPool2d(3, stride=2, padding=1, count_include_pad=False)
-        self.comb_iter_2_right = BranchSeparables(out_channels_right, out_channels_right, 5, 2, 2, bias=False)
+        self.comb_iter_2_left = AvgPool(pad=pad)
+        self.comb_iter_2_right = BranchSeparables(out_channels_right, out_channels_right, 5, 2, 2, bias=False, reduction=pad)
 
         self.comb_iter_3_right = nn.AvgPool2d(3, stride=1, padding=1, count_include_pad=False)
 
-        self.comb_iter_4_left = BranchSeparables(out_channels_right, out_channels_right, 3, 1, 1, bias=False)
-        self.comb_iter_4_right = nn.MaxPool2d(3, stride=2, padding=1)
+        self.comb_iter_4_left = BranchSeparables(out_channels_right, out_channels_right, 3, 1, 1, bias=False, reduction=pad)
+        self.comb_iter_4_right = MaxPool(pad=pad)
 
     def forward(self, x, x_prev):
         x_left = self.conv_prev_1x1(x_prev)
@@ -540,8 +478,9 @@ class NASNet(nn.Module):
             self.block1.append(next_cell)
 
         out_ch = nf*fs
-        self.reduction_cell_0 = ReductionCell0(in_channels_left=in_ch, out_channels_left=out_ch,
-                                               in_channels_right=in_ch, out_channels_right=out_ch)
+        self.reduction_cell_0 = ReductionCell(in_channels_left=in_ch, out_channels_left=out_ch,
+                                              in_channels_right=in_ch, out_channels_right=out_ch,
+                                              pad=True)
 
         cell_idx += 1
         next_cell = NormalCell(in_channels_left=in_ch, out_channels_left=out_ch//fs,
@@ -558,8 +497,8 @@ class NASNet(nn.Module):
 
 
         out_ch = nf*fs*2
-        self.reduction_cell_1 = ReductionCell1(in_channels_left=in_ch, out_channels_left=out_ch,
-                                               in_channels_right=in_ch, out_channels_right=out_ch)
+        self.reduction_cell_1 = ReductionCell(in_channels_left=in_ch, out_channels_left=out_ch,
+                                              in_channels_right=in_ch, out_channels_right=out_ch)
 
         cell_idx += 1
         next_cell = NormalCell(in_channels_left=in_ch, out_channels_left=out_ch//fs,
